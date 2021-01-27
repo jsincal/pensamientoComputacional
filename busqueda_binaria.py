7b9327802edbc59fa14407220e466cc7b9fde369
Jorge Sincal <sincaljorge@outlook.com>
2021-01-26 23:28:44 -0600

    agregar busqueda binaria

---
+++ busqueda_binaria.py
@@ -0,0 +1,20 @@
+def run():
+    objetivo = int(input('Escoge un numero: '))
+    epsilon = 0.001
+    bajo = 0.0
+    alto = max(1.0, objetivo)
+    respuesta = (alto + bajo) / 2
+
+    while abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'bajo={bajo}, alto={alto}, Respuesta={respuesta}')
+        if respuesta**2 < objetivo:
+            bajo = respuesta
+        else:
+            alto = respuesta
+
+        respuesta = (alto + bajo) /2
+    
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+
+if __name__ == '__main__':
+    run()
